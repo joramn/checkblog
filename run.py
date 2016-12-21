58ce6498d69b43d786640b7b60795111086cb67e
Joram Ngwenya <joram.ngwenya@gmail.com>
2016-12-21 17:48:55 -0500

    modified run and Procfile added'

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from blog import app
 import os
 
-app.secret_key = os.urandom(25)
-app.run(debug=True)
+port = int(os.environ.get('PORT', 5000))
+app.secret_key = os.urandom(24)
+app.run(host='0.0.0.0',port=port)
